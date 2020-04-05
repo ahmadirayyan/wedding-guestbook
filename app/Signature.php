52642ad828dce17aa21c90d1dbb04fea118ad647
ahmadirayyan <ahmadirayyan@gmail.com>
2020-04-05 20:29:32 +0800

    backend api index, show, store, report

--- app/Signature.php
+++ app/Signature.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Signature extends Model
 {
     protected $fillable = ['name', 'email', 'body', 'flagged_at'];
+
+    public function scopeIgnoreFlagged($query)
+    {
+        return $query->where('flagged_at', null);
+    }
+    
+    public function flag()
+    {
+        return $this->update(['flagged_at' => \Carbon\Carbon::now()]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::resource('signatures', 'Api\SignatureController')
+    ->only(['index', 'store', 'show']);
+
+Route::put('signatures/{signature}/report', 'Api\ReportSignature@update');
