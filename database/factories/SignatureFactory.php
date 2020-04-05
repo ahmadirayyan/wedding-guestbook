c9c9b92a16320efd86e683b616808e7d927aac60
ahmadirayyan <ahmadirayyan@gmail.com>
2020-04-05 14:49:06 +0800

    database migrate and factory

---
+++ database/factories/SignatureFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Signature::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->safeEmail,
+        'body' => $faker->sentence
+    ];
+});
