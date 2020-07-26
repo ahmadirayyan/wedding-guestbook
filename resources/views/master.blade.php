fd013d753215b674c03da8383067e3144cfc95d4
ahmadirayyan <ahmadirayyan@gmail.com>
2020-07-26 19:17:13 +0800

    change layout

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,22 +10,6 @@
 </head>
 <body sty>
     <div id="app">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-            <div class="container">
-                <a class="navbar-brand" href="{{ route('home') }}">AA's Wedding Guests</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerMenu" aria-controls="navbarTogglerMenu" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarTogglerMenu">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
-                           <button type="button" class="btn btn-outline-light my-2 my-sm-0" data-toggle="modal" data-target="#fillModal">Sign Here</button>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
         @yield('content')
 
         <signature-form></signature-form>
--- resources/views/signatures/index.blade.php
+++ resources/views/signatures/index.blade.php
@@ -7,45 +7,53 @@
 @endsection
 
 @section('content')
-    <section id="CarouselWedding">
-        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="d-block w-100" src="{{ url('/images/mb1.jpg') }}" alt="First slide">
-                    <div class="carousel-caption d-none d-md-block">
-                        <a href="#GuestSignature" class="btn btn-outline-light my-2 my-sm-0">Our Special Guests</a>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6" style="padding-right:0px">
+                <section id="CarouselWedding">
+                    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img class="d-block w-100" src="{{ url('/images/mb1.jpg') }}" alt="First slide">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <a href="#GuestSignature" class="btn btn-outline-light my-2 my-sm-0">Sign Here</a>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="{{ url('/images/mb2.jpg') }}" alt="Second slide">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <a href="#GuestSignature" class="btn btn-outline-light my-2 my-sm-0">Sign Here</a>
+                                </div>
+                            </div>
+                            <div class="carousel-item">
+                                <img class="d-block w-100" src="{{ url('/images/mb3.jpg') }}" alt="Third slide">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <button type="button" class="btn btn-outline-light my-2 my-sm-0" data-toggle="modal" data-target="#fillModal">Sign Here</button>
+                                </div>
+                            </div>
+                        </div>
+                        <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
                     </div>
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="{{ url('/images/mb2.jpg') }}" alt="Second slide">
-                    <div class="carousel-caption d-none d-md-block">
-                        <a href="#GuestSignature" class="btn btn-outline-light my-2 my-sm-0">Our Special Guests</a>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="{{ url('/images/mb3.jpg') }}" alt="Third slide">
-                    <div class="carousel-caption d-none d-md-block">
-                        <a href="#GuestSignature" class="btn btn-outline-light my-2 my-sm-0">Our Special Guests</a>
-                    </div>
-                </div>
+                </section>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
-    </section>
-    <section id="GuestSignature" style="padding-top: 54px;">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <signatures></signatures>
-                </div>
+            <div class="col-md-6 scrollable">
+                <section id="GuestSignature">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <signatures></signatures>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
         </div>
-    </section>
+    </div>
 @endsection
